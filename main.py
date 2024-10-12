633b6c5fad768fecb39085af323de26328d113c8
AnonymousPerson-0 <122753028+AnonymousPerson-0@users.noreply.github.com>
2024-10-11 20:09:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import tkinter
+import pygame
 import sys
 import math
 import os
 
+pygame.init()
+pygame.display.init()
+pygame.Mixer.init()
+# if someone can find a w background track that'd be lit
+
+def main():
+    # i prefer pygame honestly.
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    # Tk(classname = 'Cost Lower Bound Estimation')
+    # mainloop()
+    
+    break
+
 if (__name__ == "__main__"):
     # event based.
+    main()
+    sys.exit()
